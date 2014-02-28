e5ea98ef6f1e0428ac81d595e0d5b6a0be6eaa06
Thomas Kienlen <kommander@laposte.net>
2014-02-28 13:21:01 +0100

    initial scaffold for folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bookmarks::Application.routes.draw do
+  resources :folders
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
