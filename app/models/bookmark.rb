be0c7dd5e977fe0b6b6bcd1e35a2835730a79e2b
Thomas Kienlen <kommander@laposte.net>
2014-03-10 14:01:05 +0100

    Bookmark..folders

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,11 @@
 class Bookmark < ActiveRecord::Base
   belongs_to :folder
+
+  def folders
+    folder.full_path
+  end
+
+  def folders=(path)
+    logger.debug "yo"
+  end
 end
--- app/models/folder.rb
+++ app/models/folder.rb
@@ -3,4 +3,11 @@ class Folder < ActiveRecord::Base
   has_many :sub_folders, :class_name => 'Folder', :inverse_of => :parent_folder, :foreign_key => 'parent_folder_id'
   has_many :bookmarks
 
+  def ancestors
+    parent_folder.nil? ? [name] : parent_folder.ancestors.dup.insert(-1, name)
+  end
+
+  def full_path(char = '/')
+    ancestors.join(char)
+  end
 end
