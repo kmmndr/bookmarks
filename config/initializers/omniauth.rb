5e9c016a388f4d09515a2b7e09136d198d6237f6
Thomas Kienlen <kommander@laposte.net>
2014-03-19 13:59:24 +0100

    omniauth gplus

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :gplus, ENV['GPLUS_KEY'], ENV['GPLUS_SECRET'], scope: 'userinfo.email, userinfo.profile'
 end
